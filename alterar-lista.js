b37a30bec825b329bc8b5b85e596346dee35f7f3
Victor Ozoterio <victorozoterio@gmail.com>
2024-07-18 16:58:38 -0300

    feat: excluindo elementos especificos do array e adicionando outros com splice

---
+++ alterar-lista.js
@@ -0,0 +1,7 @@
+const listaEstudantes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+listaEstudantes.splice(0, 2, "Rodrigo", "Sofia");
+// listaEstudantes.push('Rodrigo');
+// listaEstudantes.push('Sofia');
+
+console.log(listaEstudantes);
