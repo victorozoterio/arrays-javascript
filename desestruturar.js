79c868a06e4bd8bfbc0ed6204c8a4aac81d8d592
Victor Ozoterio <victorozoterio@gmail.com>
2024-07-19 09:35:40 -0300

    feat: desestruturando um array de arrays

---
+++ desestruturar.js
@@ -0,0 +1,20 @@
+const alunos = ["João", "Juliana", "Caio", "Ana"];
+const medias = [10, 8, 7.5, 9];
+
+const lista = [alunos, medias];
+
+function exibeNomeENota(aluno) {
+	if (lista[0].includes(aluno)) {
+		// desestrutura a lista
+		const [alunos, medias] = lista;
+		//
+		const indice = alunos.indexOf(aluno);
+		const mediaAluno = medias[indice];
+		console.log(`${aluno} tem a média ${mediaAluno}`);
+	} else {
+		console.log("Estudante não existe na lista");
+	}
+}
+
+exibeNomeENota("Juliana");
+exibeNomeENota("Mari");
