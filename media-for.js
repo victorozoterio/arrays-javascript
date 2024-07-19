bc7742f77618c97ed1a1c1deb459a1a72998aadb
Victor Ozoterio <victorozoterio@gmail.com>
2024-07-19 10:25:44 -0300

    feat: obtendo média com for

---
+++ media-for.js
@@ -0,0 +1,10 @@
+const notas = [10, 6.5, 8, 7.5];
+
+let somaDasNotas = 0;
+
+for (let indice = 0; indice < notas.length; indice++) {
+	somaDasNotas += notas[indice];
+}
+
+const media = somaDasNotas / notas.length;
+console.log(`a média das notas é ${media}`);
